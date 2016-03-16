8bb98f2e7a337aa21969994ac3be9a0ea7111bf3
Rikard <Rikard>
2016-03-16 23:41:13 +0100

    Final files and styling

---
+++ parts/loop-downloads.php
@@ -0,0 +1,20 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('large-12 column box-3035'); ?> role="article" style="background-color: #fefefe; padding: 1rem; margin-bottom: 1rem;" itemscope itemtype="http://schema.org/WebPage">
+						
+	<header class="article-header">
+		<h1 class="page-title"><?php the_title(); ?></h1>
+	</header> <!-- end article header -->
+					
+    <section class="entry-content" itemprop="articleBody">
+    	<div class="row small-up-1 medium-up-2 large-up-3">
+	    <?php the_content(); ?>
+	</div>
+	    <?php wp_link_pages(); ?>
+	</section> <!-- end article section -->
+						
+	<footer class="article-footer">
+		
+	</footer> <!-- end article footer -->
+						    
+	<?php comments_template(); ?>
+					
+</article> <!-- end article -->
